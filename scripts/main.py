8b013125ed432d2d1f62772224172c972a9ff153
larswillems <lars.willems30@gmail.com>
2023-07-12 17:54:28 +0200

    making repo

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,8 @@ from helper import deleteTemporaryFiles, deleteFiles
 from redditScraper import getContent
 
 print("delete previous output videos")
-deleteFiles("../output")
+#deleteFiles("../output")
 
 getContent(1)
 
-deleteTemporaryFiles()
+#deleteTemporaryFiles()
